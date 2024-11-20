a60c04f7a1f2c4f3826945324a89a48917d84697
kehinde_Daniels <kehindedaniels501@gmail.com>
2024-11-20 21:34:38 +0100

    feat: added login-page.

--- src/pages/login-page/index.tsx
+++ src/pages/login-page/index.tsx
@@ -1,7 +1,70 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./login.scss";
+import { LogInIllustrationImage, logoicon } from "../../assets";
 const Login: React.FC = () => {
-  return <div className="login-page">login page</div>;
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (event: React.FormEvent) => {
+    event.preventDefault();
+    navigate("/user");
+  };
+
+  const handleDemoLogin = () => {
+    navigate("/user");
+  };
+
+  return (
+    <div className="login-page">
+      <div className="logo">
+        <img src={logoicon} alt="Logo" />
+      </div>
+      <div className="content">
+        <div className="illustration">
+          <img
+            className="illustration"
+            src={LogInIllustrationImage}
+            alt="Login Illustration"
+          />
+        </div>
+
+        <div className="form">
+          <div className="welcome">
+            <h1>Welcome</h1>
+            <p>Enter details to login</p>
+          </div>
+          <form onSubmit={handleLogin}>
+            <div>
+              <label htmlFor="email"></label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email: Kindly use the login button directly"
+              />
+            </div>
+            <div className="password">
+              <label htmlFor="password"></label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password: Kindly use the login button directly"
+              />
+              <p>Forgot Password?</p>
+            </div>
+            <button onClick={handleDemoLogin} style={{ marginTop: "10px" }}>
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
